22b5d2ecfc1ff69edfe61d6ba5d8b00363732f59
Alex Zo <zhoul103@mcmaster.ca>
2024-11-21 21:19:19 -0500

    Updated for GameMechs.cpp

--- GameMechs.cpp
+++ GameMechs.cpp
@@ -18,59 +18,59 @@ GameMechs::~GameMechs()
 
 bool GameMechs::getExitFlagStatus() const
 {
-
+    return exitFlag;
 }
 
 bool GameMechs::getLoseFlagStatus() const
 {
-
+    return loseFlag;
 }
     
 
 char GameMechs::getInput() const
 {
-
+    return input;
 }
 
 int GameMechs::getScore() const
 {
-
+    return score;
 }
 
 void GameMechs::incrementScore()
 {
-    
+    score++;
 }
 
 int GameMechs::getBoardSizeX() const
 {
-
+    return boardSizeX;
 }
 
 int GameMechs::getBoardSizeY() const
 {
-
+    return boardSizeY;
 }
 
 
 void GameMechs::setExitTrue()
 {
-
+    exitFlag = 1;
 }
 
 void GameMechs::setLoseFlag()
 {
-    
+    loseFlag = 1;
 }
 
 void GameMechs::setInput(char this_input)
 {
-
+    this_input = input;
 }
 
 void GameMechs::clearInput()
 {
-
+    
 }
 
 // More methods should be added here
